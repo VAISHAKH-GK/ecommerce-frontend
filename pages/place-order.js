7b5a8f9b0047f05eaa4a24919533c04b6d1e59d9
Vaishakh GK <vaishakhgk2006@gmail.com>
2022-09-10 23:14:01 +0530

    designed place-order page

--- pages/place-order.js
+++ pages/place-order.js
@@ -31,7 +31,123 @@ export default function PlaceOrder({ isLoggedIn }) {
     <div>
       <NavBar user={user} userType='normal' />
       <div className='container'>
-        <h1>Place Order Page</h1>
+        <section className=' col-md-12' style={{ 'margin-top': '20vh' }}>
+          <form action='' id='payment' method='post'>
+            <div className='row'>
+              <div className='  col-md-5'>
+                <div className='mb-3'>
+                  <h1 className='mx-auto' style={{ width: '370px' }}>
+                    Order Details
+                  </h1>
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor='Name' className='form-label'>
+                    Name
+                  </label>
+                  <input
+                    type='text'
+                    className='form-control'
+                    id='Name'
+                    placeholder='Enter your name'
+                    name='Name'
+                  />
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor='Address' className='form-label'>
+                    Address
+                  </label>
+                  <input
+                    type='text'
+                    className='form-control'
+                    id='Address'
+                    placeholder='Shippment Address'
+                    name='Address'
+                  />
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor='Email' className='form-label'>
+                    Email Address
+                  </label>
+                  <input
+                    type='email'
+                    className='form-control'
+                    id='Email'
+                    placeholder='Email Address'
+                    name='Email'
+                  />
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor='Mobile' className='form-label'>
+                    Mobile
+                  </label>
+                  <input
+                    type='tel'
+                    className='form-control'
+                    id='Mobile'
+                    placeholder='Mobile Number'
+                    name='Mobile'
+                  />
+                  <input
+                    type='text'
+                    name='userId'
+                    id=''
+                    value='{{user._id}}'
+                    hidden
+                  />
+                </div>
+              </div>
+              <div className='col-md-5 ml-auto'>
+                <div
+                  className='solid '
+                  style={{
+                    ' border': '2px solid',
+                    'border-radius': '10px',
+                    'border-color': 'grey',
+                    padding: '20px',
+                    height: '260px',
+                  }}
+                >
+                  <div>
+                    <h3 className='font-weight-bold'>Total Amount : ₹100</h3>
+                  </div>
+                  <hr />
+                  <div>
+                    <h5
+                      style={{
+                        'padding-bottom': '10px',
+                        'padding-top': '10px',
+                      }}
+                    >
+                      Payment method
+                    </h5>
+                    <input
+                      type='radio'
+                      id='COD'
+                      name='pay_method'
+                      value='COD'
+                    />
+                    <label htmlFor='COD'>COD</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Online'
+                      name='pay_method'
+                      value='ONLINE'
+                    />
+                    <label htmlFor='Online'>Online</label>
+                    <br />
+                    <button
+                      className='float-right btn btn-primary'
+                      type='submit'
+                    >
+                      Checkout
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </section>
       </div>
     </div>
   )
